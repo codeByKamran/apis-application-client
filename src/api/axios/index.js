7f47624a8a606c1594c15302103fb2380eb09917
Kamran Ashraf <79129427+codeByKamran@users.noreply.github.com>
2022-12-21 01:07:32 +0500

    heroku replaced with adaptable

--- src/api/axios/index.js
+++ src/api/axios/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://api-archive-server.herokuapp.com/api/v1"
+      ? "https://api-archive-backend.adaptable.app/api/v1"
       : "http://localhost:3500/api/v1",
 });
 
